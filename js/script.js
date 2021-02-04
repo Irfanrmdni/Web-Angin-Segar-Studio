3698d17d7edf5f2d2fd2935f290fa309608e05ea
irfanrmdni <irfanrmdni209@gmail.com>
2021-02-04 16:51:11 +0700

    mengatur responsive tablet

--- js/script.js
+++ js/script.js
@@ -1,41 +1,90 @@
-const topbarEl = document.getElementById('topbar');
-const toTopEl = document.getElementById('toTop');
-const toggleNavbarEl = document.getElementById('toggle-navbar');
-const navbarUlEl = document.querySelector('.navbar-link');
-const navbarEl = document.querySelector('.navbar');
+const topbar = document.querySelector('.topbar');
+const navbarLink = document.querySelector('.navbar-link');
+const toTop = document.querySelector('.toTop');
+const burgerMenu = document.getElementById('toggle-navbar');
+const navbar = document.querySelector('.navbar');
 
-const aboutEl = document.getElementById('about');
-const servicesEl = document.getElementById('services');
-const ourworksEl = document.getElementById('ourworks');
-const footerEl = document.getElementById('footer');
+burgerMenu.addEventListener('click', function () {
+    navbarLink.classList.toggle('active-navbar');
+});
+
+toTop.addEventListener('click', function () {
+    window.scrollTo(0, 0);
+});
 
 window.addEventListener('scroll', function () {
-    const scrollY = window.pageYOffset;
 
-    scrollY > 500 ? toTopEl.classList.add('active-toTop') : toTopEl.classList.remove('active-toTop');
+    if (window.pageYOffset > 300) {
+        toTop.classList.add('active-toTop');
+    } else {
+        toTop.classList.remove('active-toTop');
+    }
 
-    toTopEl.addEventListener('click', function (e) {
-        e.preventDefault();
-        window.scrollTo(0, 0);
-    });
+    if (window.pageYOffset > 0) {
+        topbar.classList.add('active-topbar');
+        navbar.classList.add('active-navbar');
+    } else {
+        topbar.classList.remove('active-topbar');
+        navbar.classList.remove('active-navbar');
+    }
 
-    scrollY > 0 ? topbarEl.classList.add('active-topbar') : topbarEl.classList.remove('active-topbar');
+    let wscroll = $(this).scrollTop();
 
-    scrollY > 0 ? navbarEl.classList.add('active-navbar') : navbarEl.classList.remove('active-navbar');
+    if (wscroll > $('.option').offset().top - 400) {
+        $('.option').addClass('active-option');
+    } else {
+        $('.option').removeClass('active-option');
+    }
+
+    if (wscroll > $('.about').offset().top - 400) {
+        $('.about').addClass('active-about');
+    } else {
+        $('.about').removeClass('active-about');
+    }
+
+    if (wscroll > $('.services').offset().top - 400) {
+        $('.services').addClass('active-services');
+    } else {
+        $('.services').removeClass('active-services');
+    }
+
+    if (wscroll > $('.ourworks').offset().top - 400) {
+        $('.ourworks').addClass('active-ourworks');
+    } else {
+        $('.ourworks').removeClass('active-ourworks');
+    }
+
+    if (wscroll > $('.client').offset().top - 400) {
+        $('.client').addClass('active-client');
+    } else {
+        $('.client').removeClass('active-client');
+    }
+
+    if (wscroll > $('.testimonial').offset().top - 400) {
+        $('.testimonial').addClass('active-testimonial');
+    } else {
+        $('.testimonial').removeClass('active-testimonial');
+    }
+
+    if (wscroll > $('.awards').offset().top - 400) {
+        $('.awards').addClass('active-awards');
+    } else {
+        $('.awards').removeClass('active-awards');
+    }
 
 });
+
 
 $('.page-scroll').on('click', function (e) {
-    e.preventDefault();
 
-    let tujuan = $(this).attr('href');
-    let elemenTujuan = $(tujuan);
+    var tujuan = $(this).attr('href');
 
-    $('body,html').animate({
+    var elemenTujuan = $(tujuan);
+
+    $('html,body').animate({
         scrollTop: elemenTujuan.offset().top - 100
-    }, 1250, 'easeInOutExpo')
+    }, 1250, 'easeInOutExpo');
+
+    e.preventDefault();
 });
 
-toggleNavbarEl.addEventListener('click', function () {
-    navbarUlEl.classList.toggle('active-navbar');
-});
